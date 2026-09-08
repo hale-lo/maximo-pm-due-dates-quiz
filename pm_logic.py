22fc7908ea05d3da68d7d82f25fbfe0fe5677687
hatt <hattrickhale@gmail.com>
2026-09-08 19:04:03 +0100

    Implement resolve_job_plan (green)

--- pm_logic.py
+++ pm_logic.py
@@ -1,2 +1,11 @@
 def resolve_job_plan(counter, sequence):
-    return None
+    best_jobplan = None
+    largest_interval = 0
+
+    for jobplan, interval in sequence.items():
+        if counter % interval == 0:
+            if interval > largest_interval:
+                largest_interval = interval
+                best_jobplan = jobplan
+
+    return best_jobplan
--- tests/test_pm_logic.py
+++ tests/test_pm_logic.py
@@ -3,13 +3,20 @@ from pm_logic import resolve_job_plan
 
 class PMLogicTest(unittest.TestCase):
     def test_resolve_job_plan(self):
+        sequence_entries = {
+            "JP11430A": 1,
+            "JP11430": 4
+        }
 
-        sequence_entries = [{"jobplan": "JP11430A", "interval": 1}, {"jobplan": "JP11430", "interval": 4}]
         self.assertEqual(resolve_job_plan(5, sequence_entries), "JP11430A")
 
     def test_resolve_job_plan_largest(self):
 
-        sequence_entries = [{"jobplan": "JP11430A", "interval": 1}, {"jobplan": "JP11430", "interval": 4}]
+        sequence_entries = {
+            "JP11430A": 1,
+            "JP11430": 4
+        }
+        
         self.assertEqual(resolve_job_plan(8, sequence_entries), "JP11430")
 
 if __name__ == "__main__":
