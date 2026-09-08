b788c8b1c1e482d5ff2a3ab72388018a3f317989
hatt <hattrickhale@gmail.com>
2026-09-08 22:58:52 +0100

    Added tests for question count and unique ids (green)

--- tests/test_question_bank.py
+++ tests/test_question_bank.py
@@ -41,3 +41,43 @@ class TestQuestionBank(unittest.TestCase):
                 11430,
                 [2, 3]
             )
+
+    def test_generate_question_bank_count_and_unique_ids(self):
+        questions = generate_question_bank(
+            number_of_questions=10,
+            seed=42
+        )
+
+        self.assertEqual(len(questions), 10)
+
+        asset_numbers = [
+            question["asset_name"].split("-")[-1]
+            for question in questions
+        ]
+
+        pm_ids = [
+            question["pm_id"]
+            for question in questions
+        ]
+
+        jobplan_ids = []
+
+        for question in questions:
+            jobplan_ids.extend(
+                question["sequence"].keys()
+            )
+
+        self.assertEqual(
+            len(asset_numbers),
+            len(set(asset_numbers))
+        )
+
+        self.assertEqual(
+            len(pm_ids),
+            len(set(pm_ids))
+        )
+
+        self.assertEqual(
+            len(jobplan_ids),
+            len(set(jobplan_ids))
+        )
