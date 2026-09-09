0a6a251f89beb29e2195f7e84d366799e5d0bd4e
hatt <hattrickhale@gmail.com>
2026-09-09 14:38:06 +0100

    Implement a landing screen and general design

--- main.py
+++ main.py
@@ -1,10 +1,195 @@
 import tkinter as tk
+from tkinter import ttk
+import ctypes
+
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
 class QuizApp(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Maximo - PM Due Dates - Quiz")
-        self.geometry('1200x800')
+
+        self.title("Quiz - Maximo - PM Due Dates")
+        self.geometry("1440x1024")
+
+        self.title_font = ("Segoe UI", 22, "bold")
+        self.instruction_font = ("Segoe UI", 12)
+        self.instruction_bold_font = ("Segoe UI", 12, "bold")
+        self.button_font = ("Segoe UI", 9, "bold")
+
+        self.bg_colour = "#F2EFEC"
+        self.connected_colour = "#D1C6BD"
+        self.primary_colour = "#B09D8E"
+        self.accent_colour = "#2A231D"
+        self.active_colour = "#3B3129"
+        self.correct_colour = "#1E573F"
+        self.error_colour = "#5D2222"
+
+        self.resizable(False, False)
+        self.configure(bg=self.bg_colour)
+
+        hwnd = ctypes.windll.user32.GetParent(self.winfo_id())
+
+        value = ctypes.c_int(1)
+
+        ctypes.windll.dwmapi.DwmSetWindowAttribute(
+            hwnd,
+            20,
+            ctypes.byref(value),
+            ctypes.sizeof(value)
+        )
+
+        self.build_landing_screen()
+
+    def empty_screen(self):
+        for widget in self.winfo_children():
+            widget.destroy()
+
+
+    def reset_screen(self):
+        self.empty_screen()
+
+        self.configure(bg=self.bg_colour)
+
+        self.menu_frame = tk.Frame(
+            self,
+            bg=self.primary_colour,
+            width=200
+        )
+
+        self.menu_frame.pack(
+            side="left",
+            padx=(30, 0),
+            pady=(30, 0),
+            fill="y"
+        )
+
+        self.menu_frame.pack_propagate(False)
+
+        tk.Label(
+            self.menu_frame,
+            text="MAXIMO",
+            font=self.title_font,
+            bg=self.primary_colour,
+            fg=self.bg_colour
+        ).pack(
+            pady=(30, 0)
+        )
+
+        self.content_frame = tk.Frame(
+            self,
+            bg=self.bg_colour
+        )
+
+        self.content_frame.pack(
+            side="left",
+            fill="both",
+            expand=True
+        )
+
+        tk.Label(
+            self.content_frame,
+            text=": PM Due Dates",
+            font=self.title_font,
+            bg=self.connected_colour,
+            fg=self.accent_colour,
+            anchor="w"
+        ).pack(
+            fill="x",
+            pady=(60, 0)
+        )
+
+    def create_menu_button(self, text, command, pady=(10, 0)):
+        tk.Button(
+            self.menu_frame,
+            text=text,
+            font=self.button_font,
+            bg=self.accent_colour,
+            fg=self.bg_colour,
+            activebackground=self.active_colour,
+            relief="flat",
+            bd=0,
+            highlightthickness=0,
+            width=20,
+            height=2,
+            cursor="hand2",
+            command=command
+        ).pack(
+            pady=pady
+        )
+
+    def build_landing_screen(self):
+        self.reset_screen()
+
+        self.create_menu_button(
+            "Start Quiz",
+            self.build_name_screen,
+            pady=(30, 0)
+        )
+
+        self.create_menu_button(
+            "View Leaderboard",
+            self.build_leaderboard_screen
+        )
+
+        tk.Label(
+            self.content_frame,
+            text="Instructions",
+            font=self.title_font,
+            bg=self.bg_colour,
+            fg=self.accent_colour
+        ).pack(
+            padx=30,
+            pady=(5, 0),
+            anchor="w"
+        )
+
+        tk.Label(
+            self.content_frame,
+            text=(
+                "You will be shown a series of ten questions. "
+                "For each question, you must review the last completed date, "
+                "job plans and frequencies. Use this information to determine "
+                "when the next maintenance activity is due and which job plan "
+                "should be applied.\n\n"
+                "There are two equations you need to know:\n"
+            ),
+            font=self.instruction_font,
+            bg=self.bg_colour,
+            fg=self.accent_colour,
+            wraplength=900,
+            justify="left",
+            anchor="w"
+        ).pack(
+            padx=30,
+            pady=(5, 0),
+            anchor="w"
+        )
+
+        tk.Label(
+            self.content_frame,
+            text=(
+                "Next frequency:\n"
+                "   Use the largest interval where counter % interval = 0.\n\n"
+                "Next due date:\n"
+                "   Last completed date + frequency (calendar months)"
+            ),
+            font=self.instruction_bold_font,
+            bg=self.bg_colour,
+            fg=self.error_colour,
+            wraplength=900,
+            justify="left",
+            anchor="w"
+        ).pack(
+            padx=30,
+            pady=(5, 0),
+            anchor="w"
+        )
+
+    def build_name_screen(self):
+        self.reset_screen()
+
+    def build_leaderboard_screen(self):
+        self.reset_screen()
 
 if __name__ == "__main__":
     app = QuizApp()
